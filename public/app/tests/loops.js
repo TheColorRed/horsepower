b14cddf748aa33faef66ff68b37befe6922a2392
Ryan Naddy <rnaddy@corp.acesse.com>
2018-03-08 17:14:26 -0600

    Start of templating rewrite

--- public/app/tests/loops.js
+++ public/app/tests/loops.js
@@ -1,31 +1,51 @@
 hp.observe('.loop-a', class extends hp.template {
   bind() {
     return {
-      template: `<div hp-for="index from 0 through 10">{{index}}</div>`
+      template: `<div hp-for="main in :data">
+        {{main.name}}
+        <div hp-for="child in main.children">
+          {{child.name}}
+        </div>
+      </div>`,
+      data: [{
+        name: 'Billy',
+        children: [{ name: 'Tom' }, { name: 'Mark' }, { name: 'Beth' }]
+      }, {
+        name: 'Jill',
+        children: [{ name: 'Sally' }, { name: 'Sue' }]
+      }]
     }
   }
 })
 
-hp.observe('.loop-b', class extends hp.template {
-  bind() {
-    return {
-      template: `<div hp-for="index from 1 to 10">{{index}}</div>`
-    }
-  }
-})
+// hp.observe('.loop-a', class extends hp.template {
+//   bind() {
+//     return {
+//       template: `<div hp-for="index from 0 through 10">{{index}}</div>`
+//     }
+//   }
+// })
 
-hp.observe('.loop-c', class extends hp.template {
-  bind() {
-    return {
-      template: `<div hp-for="index from 0 through 20 by 2">{{index}}</div>`
-    }
-  }
-})
+// hp.observe('.loop-b', class extends hp.template {
+//   bind() {
+//     return {
+//       template: `<div hp-for="index from 1 to 10">{{index}}</div>`
+//     }
+//   }
+// })
 
-hp.observe('.loop-d', class extends hp.template {
-  bind() {
-    return {
-      template: `<div hp-for="index from 20 through 0 by 2">{{index}}</div>`
-    }
-  }
-})
+// hp.observe('.loop-c', class extends hp.template {
+//   bind() {
+//     return {
+//       template: `<div hp-for="index from 0 through 20 by 2">{{index}}</div>`
+//     }
+//   }
+// })
+
+// hp.observe('.loop-d', class extends hp.template {
+//   bind() {
+//     return {
+//       template: `<div hp-for="index from 20 through 0 by 2">{{index}}</div>`
+//     }
+//   }
+// })
--- src/core/template.ts
+++ src/core/template.ts
@@ -13,11 +13,19 @@ namespace hp.core {
     parent?: Element
     data?: any
   }
+
+  export interface dataScope {
+    name: string
+    location: string[]
+  }
+
   export class template {
     public static templates: templateElement[] = []
 
     public readonly template: templateElement
     private readonly comp: hp.template
+
+    private readonly dataScope: dataScope[] = []
 
     private reEvaluateTemplate: boolean = true
     public set data(value: any) { this.template.data = value }
@@ -26,7 +34,6 @@ namespace hp.core {
     public get html(): string | Element { return this.template.element }
 
     public static add(tpl: string | Element, data?: any) {
-
       this.templates.push({ element: this.toTemplate(tpl), data })
     }
 
@@ -40,10 +47,20 @@ namespace hp.core {
       this.reEvaluateTemplate = reEvaluateTemplate
     }
 
+    private getLocation(key: string) {
+      let val = this.dataScope.find(i => i.name == key)
+      return val ? val.location : []
+    }
+
+    private get last() {
+      return this.dataScope[this.dataScope.length - 1]
+    }
+
     public toString() {
       if (!this.template) return ''
       let clone = this.template.element.cloneNode(true) as Element
-      this.process(clone, this.template.data, -1, '', '', '')
+      // this.process(clone, this.template.data, -1, '', '', '')
+      this.process(clone, 0)
       clone.innerHTML = clone.innerHTML.replace(/\\{/g, '{').replace(/\\}/g, '}')
       return clone.innerHTML
     }
@@ -64,98 +81,135 @@ namespace hp.core {
       return element
     }
 
-    public process(element: Element, data: any, index: number | string, source: string, key: string, val: string) {
-      // this.bindElement(element, data, index, source, key, val)
+    public process(element: Element, index: string | number) {
       Array.from(element.children).forEach(child => {
-        let hasFor = child.hasAttribute('hp-for') || child.hasAttribute('hp-for-else')
-        if (hasFor) {
-          this.runFor(child, element, data)
-          // this.runIf(child, element, data)
+        if (child.hasAttribute('hp-for') || child.hasAttribute('hp-for-else')) {
+          this.runFor(child, element, index)
         } else {
-          this.bindElement(child, data, index, source, key, val)
-          this.process(child, data, index, source, key, val)
+
         }
+
       })
     }
 
-    private runFor(element: Element, parent: Element, data: any) {
+    // public process(element: Element, data: any, index: number | string, source: string, key: string, val: string) {
+    //   // this.bindElement(element, data, index, source, key, val)
+    //   Array.from(element.children).forEach(child => {
+    //     let hasFor = child.hasAttribute('hp-for') || child.hasAttribute('hp-for-else')
+    //     if (hasFor) {
+    //       this.runFor(child, element, data)
+    //       // this.runIf(child, element, data)
+    //     } else {
+    //       this.bindElement(child, data, index, source, key, val)
+    //       this.process(child, data, index, source, key, val)
+    //     }
+    //   })
+    // }
+
+    private runFor(element: Element, parent: Element, index: string | number) {
       if (element.hasAttribute('hp-for')) {
         let elFor = element.getAttribute('hp-for') as string
         if (elFor.indexOf(' in ') > -1) {
-          this._forObjectArray(element, parent, elFor, data)
+          this._forObjectArray(element, parent, elFor, index)
+          this.dataScope.pop()
         } else if (elFor.indexOf(' from ') > -1) {
-          this._forCount(element, parent, elFor, data)
-        }
-      } else if (element.hasAttribute('hp-for-else')) {
-        element.remove()
-        let source = element.getAttribute('hp-for-else') || ''
-        let newData = source == ':data' ? data : query(source, data)
-        if (
-          (Array.isArray(newData) && data.length == 0) ||
-          (typeof newData == 'object' && Object.keys(newData).length == 0)
-        ) {
-          this._makeNode(element, parent, data, -1, source, '', '')
+          // this._forCount(element, parent, elFor)
         }
       }
+      // else if (element.hasAttribute('hp-for-else')) {
+      //   element.remove()
+      //   let source = element.getAttribute('hp-for-else') || ''
+      //   let newData = source == ':data' ? data : query(source, data)
+      //   if (
+      //     (Array.isArray(newData) && data.length == 0) ||
+      //     (typeof newData == 'object' && Object.keys(newData).length == 0)
+      //   ) {
+      //     this._makeNode(element, parent, data, -1, source, '', '')
+      //   }
+      // }
     }
 
-    private _forCount(element: Element, parent: Element, elFor: string, data: any) {
-      let [key, from, start, type, end, by, increment] = elFor.split(/ (from|to|through|by) /i).map(i => i.trim())
-      let s = 0, e = 1, inc = 1
-      s = parseFloat(start.match(/[a-zA-Z]+/) ? data[start] : start)
-      e = parseFloat(end.match(/[a-zA-Z]+/) ? data[end] : end)
-      inc = parseFloat(increment || '1')
-      // Make sure 'from' and 'to' or 'through' are used and optionally 'by'
-      if (by) by = by.toLowerCase()
-      if (from.toLowerCase() != 'from' || ['by', undefined].indexOf(by) == -1 || ['to', 'through'].indexOf(type.toLowerCase()) == -1) return
-      element.remove()
-      // end is exclusive
-      if (type == 'to') {
-        if (e > s) {
-          // Count up
-          for (let i = s; i < e; i += inc) { this._makeNode(element, parent, data, i, '', key, '') }
-        } else {
-          // Count down
-          for (let i = s; i > e; i -= inc) { this._makeNode(element, parent, data, i, '', key, '') }
-        }
-      }
-      // end is inclusive
-      else if (type == 'through') {
-        if (e > s) {
-          // Count up
-          for (let i = s; i <= e; i += inc) { this._makeNode(element, parent, data, i, '', key, '') }
-        } else {
-          // Count down
-          for (let i = s; i >= e; i -= inc) { this._makeNode(element, parent, data, i, '', key, '') }
-        }
-      }
-    }
+    // private _forCount(element: Element, parent: Element, elFor: string, data: any) {
+    //   let [key, from, start, type, end, by, increment] = elFor.split(/ (from|to|through|by) /i).map(i => i.trim())
+    //   let s = 0, e = 1, inc = 1
+    //   s = parseFloat(start.match(/[a-zA-Z]+/) ? data[start] : start)
+    //   e = parseFloat(end.match(/[a-zA-Z]+/) ? data[end] : end)
+    //   inc = parseFloat(increment || '1')
+    //   // Make sure 'from' and 'to' or 'through' are used and optionally 'by'
+    //   if (by) by = by.toLowerCase()
+    //   if (from.toLowerCase() != 'from' || ['by', undefined].indexOf(by) == -1 || ['to', 'through'].indexOf(type.toLowerCase()) == -1) return
+    //   element.remove()
+    //   // end is exclusive
+    //   if (type == 'to') {
+    //     if (e > s) {
+    //       // Count up
+    //       for (let i = s; i < e; i += inc) { this._makeNode(element, parent, data, i, '', key, '') }
+    //     } else {
+    //       // Count down
+    //       for (let i = s; i > e; i -= inc) { this._makeNode(element, parent, data, i, '', key, '') }
+    //     }
+    //   }
+    //   // end is inclusive
+    //   else if (type == 'through') {
+    //     if (e > s) {
+    //       // Count up
+    //       for (let i = s; i <= e; i += inc) { this._makeNode(element, parent, data, i, '', key, '') }
+    //     } else {
+    //       // Count down
+    //       for (let i = s; i >= e; i -= inc) { this._makeNode(element, parent, data, i, '', key, '') }
+    //     }
+    //   }
+    // }
 
-    private _forObjectArray(element: Element, parent: Element, elFor: string, data: any) {
+    private _forObjectArray(element: Element, parent: Element, elFor: string, index: string | number) {
       let [value, source] = elFor.split(' in ').map(i => i.trim())
+      let last = this.last || { name: '', location: [] }
+      let lastLocation = last.location.join('.')
+      let lastName = last.name || ''
+      this.dataScope.push({ name: value, location: source.split('.') })
       let [arg1, arg2] = value.split(',')
       let key = arg1 && arg2 ? arg1 : ''
       let val = arg1 && !arg2 ? arg1 : arg2
-      let newData = source == ':data' ? data : query(source, data)
-      element.remove()
+
+      let s = source.split('.')
+      s.shift()
+      s.unshift(index.toString())
+      if (lastLocation != ':data')
+        s.unshift(lastLocation)
+      let loc = s.join('.').replace(/^\.|\.$/g, '')
+      // let loc = source == ':data' ? '' : lastLocation
+      // console.log({ lastLocation, lastName, loc, source, index })
+      // let newData = query(loc, this.data)
+      let newData = source == ':data' ? this.data : query(loc, this.data)
+      // console.log(newData)
       if (Array.isArray(newData)) {
         newData.forEach((data, index) => {
-          this._makeNode(element, parent, data, index, source, key, val)
+          this._makeNode(element, parent, index)
         })
-      } else if (typeof newData == 'object') {
-        for (let index in newData) {
-          this._makeNode(element, parent, data, index, source, key, val)
-        }
       }
+
+      // this.dataScope.push({ source, parent })
+      // element.remove()
+      // if (Array.isArray(newData)) {
+      //   newData.forEach((data, index) => {
+      //     this._makeNode(element, parent, data, index, source, key, val)
+      //   })
+      // } else if (typeof newData == 'object') {
+      //   for (let index in newData) {
+      //     this._makeNode(element, parent, data, index, source, key, val)
+      //   }
+      // }
     }
 
-    private _makeNode(element: Element, parent: Element, data: any, index: number | string, source: string, key: string, val: string) {
+    private _makeNode(element: Element, parent: Element, index: string | number) {//, data: any, index: number | string, source: string, key: string, val: string) {
       let clone = element.cloneNode(true) as Element
       parent.appendChild(clone)
-      clone.removeAttribute('hp-for')
-      clone.removeAttribute('hp-for-else')
-      this.bindElement(clone, data, index, source, key, val)
-      this.process(clone, data, index, source, key, val)
+      // clone.removeAttribute('hp-for')
+      // clone.removeAttribute('hp-for-else')
+      // this.bindElement(clone, data, index, source, key, val)
+      // this.bindElement(clone, data, index, source, key, val)
+      this.bindElement(clone)
+      this.process(clone, index)//, data, index, source, key, val)
     }
 
     // private runIf(element: Element, parent: Element, data: any) {
@@ -164,35 +218,42 @@ namespace hp.core {
     //   }
     // }
 
-    private bindElement(element: Element, data: any, index: number | string, source: string, key: string, val: string) {
+    private bindElement(element: Element) {
       let inlineBindings = this.inlineBindings(element)
-      let rm = ''
-      let selectorOrig = ''
-      let selector: string[] = []
-      let newSelector = ''
-      if (element.hasAttribute('hp-bind') || element.hasAttribute('hp-bind-html')) {
-        selectorOrig = element.getAttribute('hp-bind') || element.getAttribute('hp-bind-html') as string
-        selector = selectorOrig.split('.')
-        selector.length > 1 && (rm = selector.shift() || '')
-        newSelector = selector.join('.')
-        let value = selector.join('.') === key ? index : data[newSelector]
-        value = typeof value == 'function' ? value() : value.toString()
-        if (rm.length == 0 || (rm.length > 0 && rm == val)) {
-          if (component.isFormItem(element)) {
-            element.value = value
-          } else {
-            if (element.getAttribute('hp-bind-html')) {
-              element.innerHTML = value
-            } else {
-              element.textContent = value
-            }
-          }
-        }
-      }
       if (inlineBindings.length > 0) {
-        this.replaceInlineBindings(element, data, index, inlineBindings, key)
+        this.replaceInlineBindings(element, inlineBindings)//, data, index, inlineBindings, key)
       }
     }
+
+    // private bindElement(element: Element, data: any, index: number | string, source: string, key: string, val: string) {
+    //   let inlineBindings = this.inlineBindings(element)
+    //   let rm = ''
+    //   let selectorOrig = ''
+    //   let selector: string[] = []
+    //   let newSelector = ''
+    //   if (element.hasAttribute('hp-bind') || element.hasAttribute('hp-bind-html')) {
+    //     selectorOrig = element.getAttribute('hp-bind') || element.getAttribute('hp-bind-html') as string
+    //     selector = selectorOrig.split('.')
+    //     selector.length > 1 && (rm = selector.shift() || '')
+    //     newSelector = selector.join('.')
+    //     let value = selector.join('.') === key ? index : data[newSelector]
+    //     value = typeof value == 'function' ? value() : value.toString()
+    //     if (rm.length == 0 || (rm.length > 0 && rm == val)) {
+    //       if (component.isFormItem(element)) {
+    //         element.value = value
+    //       } else {
+    //         if (element.getAttribute('hp-bind-html')) {
+    //           element.innerHTML = value
+    //         } else {
+    //           element.textContent = value
+    //         }
+    //       }
+    //     }
+    //   }
+    //   if (inlineBindings.length > 0) {
+    //     this.replaceInlineBindings(element, data, index, inlineBindings, key)
+    //   }
+    // }
 
     private inlineBindings(node: Element) {
       let bindings: string[] = []
@@ -206,25 +267,49 @@ namespace hp.core {
       let matches = (n.textContent || '').match(/({{.+?}})/g)
       if (matches) bindings = bindings.concat(matches)
       return bindings
+      // return []
     }
 
-    private replaceInlineBindings(node: Element, data: any, index: any, bindings: string[], key: string) {
+    private replaceInlineBindings(node: Element, bindings: string[]) {
       bindings.forEach(binding => {
         let regexp = new RegExp(escapeRegExp(binding), 'g')
         let selector = binding.replace(/{{|}}/g, '').split('.')
-        selector.length > 1 && selector.shift()
-        let newSelector = selector.join('.')
-        let value = newSelector === key ? index : data[newSelector]
-        value = (Array.isArray(data) && newSelector === key ? data[value] : value) || ''
-        let finalValue = (typeof value == 'function' ? value() : value.toString()) as string
-        finalValue = finalValue.replace(/</g, '&lt;').replace(/>/g, '&gt;')
-        for (let i = 0; i < node.attributes.length; i++) {
-          let attr = node.attributes[i]
-          attr.value = attr.value.replace(regexp, finalValue)
-        }
-        node.innerHTML = node.innerHTML.replace(regexp, finalValue)
+        // console.log(this.getLocation(selector[0]))
+        console.log('s', selector)
+        console.log(this.getPath(selector).join('.'))
       })
     }
+
+    private getPath(items: string[]) {
+      // console.log(items)
+      let path: string[] = []
+      let item = items.shift()
+      if (item) {
+        let loc = this.getLocation(item)
+        if (loc.length > 0 && loc.join('.') != ':data') {
+          path.push(...this.getPath(loc))
+        }
+      }
+      return path
+    }
+
+    // private replaceInlineBindings(node: Element, data: any, index: any, bindings: string[], key: string) {
+    //   bindings.forEach(binding => {
+    //     let regexp = new RegExp(escapeRegExp(binding), 'g')
+    //     let selector = binding.replace(/{{|}}/g, '').split('.')
+    //     selector.length > 1 && selector.shift()
+    //     let newSelector = selector.join('.')
+    //     let value = newSelector === key ? index : data[newSelector]
+    //     value = (Array.isArray(data) && newSelector === key ? data[value] : value) || ''
+    //     let finalValue = (typeof value == 'function' ? value() : value.toString()) as string
+    //     finalValue = finalValue.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+    //     for (let i = 0; i < node.attributes.length; i++) {
+    //       let attr = node.attributes[i]
+    //       attr.value = attr.value.replace(regexp, finalValue)
+    //     }
+    //     node.innerHTML = node.innerHTML.replace(regexp, finalValue)
+    //   })
+    // }
 
     public revalidate() {
       if (!this.template.parent) return
